5a6e680fbd7cc8fb390e9a565ed9ff8632117878
Thamires <thamiipie@gmail.com>
2024-05-17 20:15:59 -0300

    criacao da entidade transacao

--- src/app/entities/transacao.py
+++ src/app/entities/transacao.py
@@ -1,3 +1,37 @@
 from typing import Tuple
 from ..errors.entity_errors import ParamNotValidated
 
+class Transacao:
+    
+    valor: float
+    timestamp: float
+    tipo: str
+
+    def __init__(self,valor: float, tipo: str, timestamp: float):
+        pass
+        
+    @staticmethod
+    def validate_valor(valor: float) -> Tuple[bool,float]:
+        if valor is None:
+            return False, "valor is required"
+        if type(valor) != float:
+            return False, "valor must be a float"
+        return True
+
+    @staticmethod
+    def validate_tipo(tipo: str) -> Tuple[bool,str]:
+        if tipo is None:
+            return False, "tipo is required"
+        if type(tipo) != str:
+            return False, "tipo must be a string"
+        return True
+        
+    
+    @staticmethod
+    def validate_timestamp(timestamp: float) -> Tuple[bool,float]:
+        if timestamp is None:
+            return False, "timestamp is required"
+        if type(timestamp) != float:
+            return False, "timestamp must be a float"
+        return True
+    
